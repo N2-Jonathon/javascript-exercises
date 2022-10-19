729a1fddf007c7c699b066686c5cc38a1ffa9068
Jonathon Quick <jonathon@spliff.li>
2022-10-19 16:49:10 +0200

    completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  celsius = (fahrenheit - 32) / 1.8;
 
+  return Math.round(celsius);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  fahrenheit = (celsius * 1.8) +32;
+  
+  return Math.round(fahrenheit);
 };
 
 // Do not edit below this line
