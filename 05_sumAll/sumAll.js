511ac285eb694886d1719e62f104cdf47a602e86
Jonathon Quick <jonathon@spliff.li>
2022-10-19 16:20:40 +0200

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+function range(start, end) {
+  var ans = []
 
+}
+
+const sumAll = function(int1, int2) {
+  var sum = 0;
+  
+  if (typeof(int1) !== "number" || typeof(int2) !== "number"){
+    return "ERROR";
+  }
+
+  if(int1 < 0 || int2 < 0){
+    return "ERROR";
+  }
+  
+  if (int1 > int2){
+    for (i=int2; i <= int1; i++){
+      sum += i;
+    }
+  }
+  else {
+    for (i=int1; i <= int2; i++){
+      sum += i;
+    }
+  }
+  return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
