accfb1af9c61306db6d5880ed977d9827c487ea8
Jonathon Quick <jonathon@spliff.li>
2022-10-19 13:16:01 +0200

    completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  charArray = Array.from(string);
+  charArray.reverse();
+  
+  return charArray.join("")
 };
 
 // Do not edit below this line
